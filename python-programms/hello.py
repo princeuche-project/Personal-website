a565825b8e43dc6b4f2b919fc17ffbcdcfa10140
Uchenna Godstime <princeuche08@gmail.com>
2022-09-13 09:33:10 +0400

    added python

---
+++ python-programms/hello.py
@@ -0,0 +1,15 @@
+color = input("please enter your favourite color:")
+print("your favourite color is:")
+print(color)
+
+food = input("please enter your best meal:")
+print("your best meal is:")
+print(food)
+
+country_capital = input("please enter the capital ciyty of your country:")
+print("The capital city of your county is:")
+print(country_capital)
+
+daily_activities = input("please tell us your daily activities:")
+print("your daily activities are:")
+print(daily_activities)
